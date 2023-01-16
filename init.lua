db120edcf2c1bf364f828be6fc0a6b1423954cb4
Daniel Indictor <daniel.indictor@gmail.com>
2023-01-16 04:36:54 -0500

    Indentation rules.

--- init.lua
+++ init.lua
@@ -3,6 +3,14 @@ autocmd FileType c,cpp setlocal equalprg=clang-format\ -style='file'
 colorscheme wombat256mod_custom
 set termguicolors
 set number
+set autoindent
+"set cindent
+set smartindent
+set expandtab
+set smarttab
+set shiftwidth=2
+set softtabstop=2
+set tabstop=2
 ]])
 
 require('plugins')
